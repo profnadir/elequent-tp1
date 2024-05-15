8c8df6bcfa5b633689ce3791b83158bb301c28d8
=profnadir <=hamzanadir.dev@gmail.com>
2024-05-15 12:08:35 +0100

    laravel debugbar

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,7 +13,9 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::with('user')->get();
+        //$articles = Article::with(['user','ville'])->get();
+        //$articles = Article::all();
         // $articles = Article::get();
         // $articles = Article::orderBy('title')->take(5)->get();
         // $articles = Article::paginate(10);
